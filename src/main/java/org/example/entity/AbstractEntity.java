9461e708b7117b887ad72a8ca9fd5ad1aa569306
makssoccer <82469846+makssoccer@users.noreply.github.com>
2022-11-12 21:46:24 +0300

    only create

---
+++ src/main/java/org/example/entity/AbstractEntity.java
@@ -0,0 +1,14 @@
+package org.example.entity;
+
+public class AbstractEntity {
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    private Long Id;
+
+}
